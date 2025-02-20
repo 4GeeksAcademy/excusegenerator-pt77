9d4c58eb0d36a9ed632d706ac44c846aeb9b8873
jdiaz1993 <diazjoseph366@gmail.com>
2025-02-20 02:04:03 +0000

    Almost done need a button

--- src/app.js
+++ src/app.js
@@ -4,8 +4,32 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateexcuse()
 };
+
+const generateexcuse = () =>  {
+
+  const whoarray = ["dad", "mom", "sister", "uncle", "long lost cousin", "twice removed twin brother"];
+  const actionarray = ["hit", "sent", "gave", "excommunicated", "broke", "ate"];
+  const whatarray = ["car", "phone", "femur", "Playstation 5", "glasses", "new leggo set"];
+  const whenarray = ["on the third friday of lent", "on the day the priest became president", "two valentines ago", "during the twighlight movie marathon", "the day the earth stood still"];
+
+
+  let whoindex = Math.floor(Math.random() * whoarray.length)
+  let actionindex = Math.floor(Math.random() * actionarray.length)
+  let whatindex = Math.floor(Math.random() * whatarray.length)
+  let whenindex = Math.floor(Math.random() * whenarray.length)
+
+  const who = document.querySelector(".who")
+  const action = document.querySelector(".action")
+  const what = document.querySelector(".what")
+  const when = document.querySelector(".when")
+
+  who.innerHTML = whoarray[whoindex]
+  action.innerHTML = actionarray[actionindex]
+  what.innerHTML = whatarray[whatindex]
+  when.innerHTML = whenarray[whenindex]
+
+};
+
